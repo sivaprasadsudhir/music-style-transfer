c2ed54f99d38a3039ab189dbefd84bfccf3e9c37
Cyclotronian <aviral.2815@gmail.com>
2018-04-23 18:13:45 -0400

    dense network created and spectrogram class updated

--- Code/autoencoder.py
+++ Code/autoencoder.py
@@ -22,68 +22,11 @@ class Autoencoder(object):
 			self.define_entire_network(input_shape, learning_r)
 
 	def define_entire_network(self, input_shape, learning_r):
-		self.input = Input(shape=input_shape)
-		print input_shape
-
-		# "encoded" is the encoded representation of the input
-		encoded = Conv2D(filters=40, kernel_size=(3, 3), strides=(3, 3),
-		                 activation='relu', padding='valid')(self.input)
-		encoded = Conv2D(filters=80, kernel_size=(2, 2),
-		                 activation='relu', padding='valid')(encoded)
-		encoded = Conv2D(filters=120, kernel_size=(2, 2), strides=(2, 2),
-		                 activation='relu', padding='valid')(encoded)
-		encoded = Conv2D(filters=200, kernel_size=(21, 47),
-		                 activation='relu', padding='valid')(encoded)
-		self.encoded=encoded
-
-		decoded = Conv2DTranspose(filters=200, kernel_size=(21, 47),
-		                          activation='relu', padding='valid')(self.encoded)
-		decoded = Conv2DTranspose(filters=120, kernel_size=(2, 2), strides=(2, 2), activation='relu', padding='valid')(decoded)
-		decoded = Conv2DTranspose(filters=80, kernel_size=(2, 2), activation='relu', padding='valid')(decoded)
-		decoded = Conv2DTranspose(filters=40, kernel_size=(3, 3), strides=(3, 3), activation='relu', padding='valid')(decoded)
-		decoded = Conv2D(filters=1, kernel_size=(1, 1), activation='relu', padding='valid')(decoded)
-		self.decoded = decoded
-
-		# this model maps an input to its reconstruction
-		self.model = Model(self.input, self.decoded)
-
-		self.model.compile(optimizer=Adam(lr=learning_r), loss='mse')
-		print_summary(self.model, line_length=80)
-
-	def save_model_weights(self, model_path):
-		self.model.save_weights(model_path)
-
-	def load_model_weights(self, model_path):
-		self.model.load_weights(model_path)
-
-	def save_model(self, model_path, encoder_path, decoder_path):
-		self.model.save(model_path)
-
-	def load_model(self, model_path, encoder_path, decoder_path):
-		self.model = load_model(model_path)
-
-class SharedAutoencoder(object):
-
-	def __init__(self, load_model=False, input_shape = None,
-	             learning_r = None, load_path = None):
-		# load or create from scratch
-		if load_model:
-			self.load_model(load_path + 'model.h5',
-							load_path + 'encoder.h5',
-							load_path + 'decoder.h5')
-		else:
-			self.define_entire_network(input_shape, learning_r)
-
-	def define_entire_network(self, input_shape, learning_r):
-		# this is the size of our encoded representations
-		# 32 floats -> compression of factor 24.5, 
-		# assuming the input is 784 floats
-		self.encoding_dim = 16
-
 		# this is our input placeholder
-		# input_shape += (1,)
 		self.input = Input(shape=input_shape)
 		print input_shape
+
+		# self.encoding_dim = 16
 
 		# "encoded" is the encoded representation of the input
 		encoded = Dense(256, activation='relu')(self.input)
@@ -93,23 +36,14 @@ class SharedAutoencoder(object):
 		self.encoded = Dense(16, activation='relu')(encoded)
 
 		# "decoded" is the lossy reconstruction of the input for instrument 1
-		decoded_1 = Dense(32, activation='relu')(self.encoded)
-		decoded_1 = Dense(64, activation='relu')(decoded_1)
-		decoded_1 = Dense(128, activation='relu')(decoded_1)
-		decoded_1 = Dense(256, activation='relu')(decoded_1)
-		self.decoded_1 = Dense(input_shape[0], activation='relu')(decoded_1)
-
-		# "decoded" is the lossy reconstruction of the input for instrument 2
-		decoded_2 = Dense(32, activation='relu')(self.encoded)
-		decoded_2 = Dense(64, activation='relu')(decoded_2)
-		decoded_2 = Dense(128, activation='relu')(decoded_2)
-		decoded_2 = Dense(256, activation='relu')(decoded_2)
-		self.decoded_2 = Dense(input_shape[0], activation='relu')(decoded_2)
-
-		self.merged_decoded = concatenate([self.decoded_1, self.decoded_2], axis=-1)
+		decoded = Dense(32, activation='relu')(self.encoded)
+		decoded = Dense(64, activation='relu')(decoded)
+		decoded = Dense(128, activation='relu')(decoded)
+		decoded = Dense(256, activation='relu')(decoded)
+		self.decoded = Dense(input_shape[0], activation='relu')(decoded)
 
 		# this model maps an input to its reconstruction
-		self.model = Model(self.input, self.merged_decoded)
+		self.model = Model(self.input, self.decoded)
 
 		self.model.compile(optimizer=Adam(lr=learning_r),
 								 loss='mse')
@@ -124,15 +58,16 @@ class SharedAutoencoder(object):
 	
 	def define_decoder_network(self):
 		# create a placeholder for an encoded (32-dimensional) input
-		# encoded_input = Input(shape=self.encoding_dim)
-		pass
+		encoded_input = Input(shape=self.encoding_dim)
+
 		# retrieve the last layer of the autoencoder model
-		# decoded_output = self.model.layers[-3](encoded_input)
-		# decoded_output = self.model.layers[-2](encoded_input)
-		# decoded_output = self.model.layers[-1](decoded_output)
+		decoded_output = self.model.layers[-4](encoded_input)
+		decoded_output = self.model.layers[-3](decoded_output)
+		decoded_output = self.model.layers[-2](decoded_output)
+		decoded_output = self.model.layers[-1](decoded_output)
 		
 		# create the decoder model
-		# self.decoder = Model(encoded_input, decoded_output)
+		self.decoder = Model(encoded_input, decoded_output)
 
 	def save_model_weights(self, model_path):
 		self.model.save_weights(model_path)
@@ -145,3 +80,4 @@ class SharedAutoencoder(object):
 
 	def load_model(self, model_path, encoder_path, decoder_path):
 		self.model = load_model(model_path)
+
--- Code/spectrogram.py
+++ Code/spectrogram.py
@@ -1,63 +1,221 @@
 from matplotlib import pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
-import sys, os
+import librosa
 import numpy as np
+import os
+import sys
+import tensorflow as tf
 
 import pdb
+
+def trim_for_encoding(wav_data, sample_length, hop_length=512):
+	"""Make sure audio is a even multiple of hop_size.
+
+	Args:
+	wav_data: 1-D or 2-D array of floats.
+	sample_length: Max length of audio data.
+	hop_length: Pooling size of WaveNet autoencoder.
+
+	Returns:
+	wav_data: Trimmed array.
+	sample_length: Length of trimmed array.
+	"""
+	if wav_data.ndim == 1:
+		# Max sample length is the data length
+		if sample_length > wav_data.size:
+			sample_length = wav_data.size
+			# Multiple of hop_length
+			sample_length = (sample_length // hop_length) * hop_length
+			# Trim
+			wav_data = wav_data[:sample_length]
+			# Assume all examples are the same length
+	elif wav_data.ndim == 2:
+		# Max sample length is the data length
+		if sample_length > wav_data[0].size:
+			sample_length = wav_data[0].size
+			# Multiple of hop_length
+			sample_length = (sample_length // hop_length) * hop_length
+			# Trim
+			wav_data = wav_data[:, :sample_length]
+
+	return wav_data, sample_length
+
+def specgram(audio,
+			 n_fft=512,
+			 hop_length=None,
+			 mask=True,
+			 log_mag=True,
+			 re_im=False,
+			 dphase=True,
+			 mag_only=False):
+	if not hop_length:
+		hop_length = int(n_fft / 2.)
+
+	fft_config = dict(
+		n_fft=n_fft, win_length=n_fft, hop_length=hop_length, center=True)
+
+	spec = librosa.stft(audio, **fft_config)
+
+	if re_im:
+		re = spec.real[:, :, np.newaxis]
+		im = spec.imag[:, :, np.newaxis]
+		spec_real = np.concatenate((re, im), axis=2)
+
+	else:
+		mag, phase = librosa.core.magphase(spec)
+		phase_angle = np.angle(phase)
+
+		# Magnitudes, scaled 0-1
+		if log_mag:
+			mag = (librosa.power_to_db(
+				mag**2, amin=1e-13, top_db=120., ref=np.max) / 120.) + 1
+		else:
+			mag /= mag.max()
+
+		if dphase:
+			#  Derivative of phase
+			phase_unwrapped = np.unwrap(phase_angle)
+			p = phase_unwrapped[:, 1:] - phase_unwrapped[:, :-1]
+			p = np.concatenate([phase_unwrapped[:, 0:1], p], axis=1) / np.pi
+		else:
+			# Normal phase
+			p = phase_angle / np.pi
+		# Mask the phase
+		if log_mag and mask:
+			p = mag * p
+		# Return Mag and Phase
+		p = p.astype(np.float32)[:, :, np.newaxis]
+		mag = mag.astype(np.float32)[:, :, np.newaxis]
+		if mag_only:
+			spec_real = mag[:, :, np.newaxis]
+		else:
+			spec_real = np.concatenate((mag, p), axis=2)
+	return spec_real
+
+def inv_magphase(mag, phase_angle):
+	phase = np.cos(phase_angle) + 1.j * np.sin(phase_angle)
+	return mag * phase
+
+def griffin_lim(mag, phase_angle, n_fft, hop, num_iters):
+	"""Iterative algorithm for phase retrival from a magnitude spectrogram.
+
+	Args:
+	mag: Magnitude spectrogram.
+	phase_angle: Initial condition for phase.
+	n_fft: Size of the FFT.
+	hop: Stride of FFT. Defaults to n_fft/2.
+	num_iters: Griffin-Lim iterations to perform.
+
+	Returns:
+	audio: 1-D array of float32 sound samples.
+	"""
+	fft_config = dict(n_fft=n_fft, win_length=n_fft, hop_length=hop, center=True)
+	ifft_config = dict(win_length=n_fft, hop_length=hop, center=True)
+	complex_specgram = inv_magphase(mag, phase_angle)
+	for i in range(num_iters):
+		audio = librosa.istft(complex_specgram, **ifft_config)
+		if i != num_iters - 1:
+			complex_specgram = librosa.stft(audio, **fft_config)
+			_, phase = librosa.magphase(complex_specgram)
+			phase_angle = np.angle(phase)
+			complex_specgram = inv_magphase(mag, phase_angle)
+	return audio
+
+
+def ispecgram(spec,
+              n_fft=512,
+              hop_length=None,
+              mask=True,
+              log_mag=True,
+              re_im=False,
+              dphase=True,
+              mag_only=True,
+              num_iters=1000):
+	"""Inverse Spectrogram using librosa.
+
+	Args:
+	spec: 3-D specgram array [freqs, time, (mag_db, dphase)].
+	n_fft: Size of the FFT.
+	hop_length: Stride of FFT. Defaults to n_fft/2.
+	mask: Reverse the mask of the phase derivative by the magnitude.
+	log_mag: Use the logamplitude.
+	re_im: Output Real and Imag. instead of logMag and dPhase.
+	dphase: Use derivative of phase instead of phase.
+	mag_only: Specgram contains no phase.
+	num_iters: Number of griffin-lim iterations for mag_only.
+
+	Returns:
+	audio: 1-D array of sound samples. Peak normalized to 1.
+	"""
+	if not hop_length:
+		hop_length = n_fft // 2
+
+	ifft_config = dict(win_length=n_fft, hop_length=hop_length, center=True)
+
+	if mag_only:
+		mag = spec[:, :, 0]
+		phase_angle = np.pi * np.random.rand(*mag.shape)
+	elif re_im:
+		spec_real = spec[:, :, 0] + 1.j * spec[:, :, 1]
+	else:
+		mag, p = spec[:, :, 0], spec[:, :, 1]
+		if mask and log_mag:
+			p /= (mag + 1e-13 * np.random.randn(*mag.shape))
+		if dphase:
+			# Roll up phase
+			phase_angle = np.cumsum(p * np.pi, axis=1)
+		else:
+			phase_angle = p * np.pi
+
+	# Magnitudes
+	if log_mag:
+		mag = (mag - 1.0) * 120.0
+		mag = 10**(mag / 20.0)
+	phase = np.cos(phase_angle) + 1.j * np.sin(phase_angle)
+	spec_real = mag * phase
+
+	if mag_only:
+		audio = griffin_lim(
+	    	mag, phase_angle, n_fft, hop_length, num_iters=num_iters)
+	else:
+		audio = librosa.core.istft(spec_real, **ifft_config)
+	return np.squeeze(audio / audio.max())
+
+def normalize(x, y):
+	return x * x + y * y
 
 class Spectrogram(object):
 
 	def __init__(self, filenames = None, spectrogram = None):
-
 		self.max_const = 1e7
+		self.n_fft = 512
+		self.hop_length = int(self.n_fft/2.)
+		self.mask = True
+		self.log_mag = True
+		self.pad = True
+		self.re_im = False
+		self.dphase = True
+		self.mag_only = False
+		self.num_iters = 1000
+		self.sample_rate = 16000
+
 		if filenames == None:
 			self.spectrogram_to_wav(spectrogram)
 		elif spectrogram == None:
 			self.wav_to_spectrogram(filenames)
 
-	def gen_both_spectograms(self, filenames):
-		# for ins in inss:
-		# inss = ["bass", "brass", "flute", "guitar", "mallet", "organ", "reed", "string", "synth_lead", "vocal"]
-		ins = "mallet"
-		keyboard = []
-		mallet = []
-		# count = 0
-		for filename in filenames:
-			new_file = filename.replace("keyboard_", ins + "_", 2)
-			# print new_file
-			if(os.path.isfile(new_file)):
-				keyboard.append(filename)
-				mallet.append(new_file)
-				# count += 1
-				# if count == 2:
-					# break
-		
-		print ins, len(keyboard), len(mallet)
-		# print keyboard, mallet
-		# exit(0)
-
-		spectrogram = []
-		for fname in keyboard:
-			sample_rate, samples = wavfile.read(fname)
-			frequencies, times, sgram = signal.spectrogram(samples, sample_rate)
-			spectrogram.append(sgram)
-		spectrogram = np.array(spectrogram) / self.max_const
-		# self.spectrogram = spectrogram.reshape(spectrogram.shape + (1,))
-		self.keyboard = spectrogram.reshape((len(spectrogram), np.prod(spectrogram.shape[1:])))
-
-		spectrogram = []
-		for fname in mallet:
-			sample_rate, samples = wavfile.read(fname)
-			frequencies, times, sgram = signal.spectrogram(samples, sample_rate)
-			spectrogram.append(sgram)
-
-		spectrogram = np.array(spectrogram) / self.max_const
-		# self.spectrogram = spectrogram.reshape(spectrogram.shape + (1,))
-		self.mallet = spectrogram.reshape((len(spectrogram), np.prod(spectrogram.shape[1:])))
-		# print self.spectrogram.shape
-
 	def wav_to_spectrogram(self, filenames):
+		spectrogram = []
+		for fname in filenames:
+			audio, _ = librosa.load(fname, sr=self.sample_rate)
+			sgram = specgram(audio)
+			# print "Shape of the spectrogram", sgram.shape
+			spectrogram.append(sgram)
+
+		# self.spectrogram = np.array(spectrogram) / self.max_const
+		self.spectrogram = np.array(spectrogram)
+
 
 		spectrogram = []
 		for fname in filenames:
@@ -70,14 +228,35 @@ class Spectrogram(object):
 		# self.spectrogram = spectrogram.reshape(spectrogram.shape + (1,))
 		self.spectrogram = spectrogram.reshape((len(spectrogram), np.prod(spectrogram.shape[1:])))
 
-	def spectrogram_to_wav(self, spectrogram):
+	def spectrogram_to_wav(self, spectrogram, filename="output.wav"):
+		dims = spectrogram.shape
+		print dims
+		# spec = spectrogram * self.max_const
+		spec = spectrogram.reshape(spectrogram.shape[1:])
+		# if self.pad:
+			# spec = tf.concat([spec, tf.zeros([1, dims[1], dims[2]])], 0)
+		audio = ispecgram(spec,
+					n_fft=self.n_fft,
+					hop_length=self.hop_length,
+					mask=self.mask,
+					log_mag=self.log_mag,
+					re_im=self.re_im,
+					dphase=self.dphase,
+					mag_only=self.mag_only,
+					num_iters=self.num_iters)
 
-		spectrogram *= self.max_const
+		librosa.output.write_wav(filename, audio, self.sample_rate,
+								 norm=False)
 
 	def visualize(self, filename, spectrogram=None):
-		print filename
-		sample_rate, samples = wavfile.read(filename)
-		frequencies, times, sgram = signal.spectrogram(samples, sample_rate)
+		audio, _ = librosa.load(filename, sr=self.sample_rate)
+		sgram = specgram(audio)
+		n_freq, n_time, unused_channels = sgram.shape
+		frequencies = range(n_freq)
+		times = range(n_time)
+
+		sgram = normalize(sgram[:, :, 0], sgram[:, :, 1])
+		sgram = np.reshape(sgram, (len(frequencies), len(times)))
 
 		ax = plt.subplot(121)
 		plt.pcolormesh(times, frequencies, sgram)
@@ -86,14 +265,20 @@ class Spectrogram(object):
 		plt.xlabel('Time [sec]')
 		ax.set_title("input")
 
-		spectrogram *= self.max_const
-		spectrogram = np.reshape(spectrogram, (len(frequencies), len(times)))
-
+		# if spectrogram != None:
+		# spectrogram *= self.max_const
+		print(spectrogram.shape)
+		spectrogram = normalize(spectrogram[0, :, :, 0], spectrogram[0, :, :, 1])
+		print(spectrogram.shape)
+		spectrogram = np.reshape(spectrogram, (len(frequencies),
+									len(times)))
+		print(spectrogram.shape)
 		ax = plt.subplot(122)
 		plt.pcolormesh(times, frequencies, spectrogram)
 		plt.imshow(spectrogram)
 		plt.ylabel('Frequency [Hz]')
 		plt.xlabel('Time [sec]')
 		ax.set_title('output')
+
 		plt.suptitle("filename: {}".format(filename))
 		plt.show()
