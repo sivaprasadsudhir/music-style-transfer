4a4424044c3a0b95018b94f1537d51e20e53bd53
Justin <justinwang.stl@gmail.com>
2018-04-26 01:42:08 -0400

    fix hard-coded batch size in loss

--- Code/loss.py
+++ Code/loss.py
@@ -4,12 +4,11 @@ from keras import backend as K
 
 def regularized_mse(y_true, y_pred):
     mse = K.mean(K.square(y_pred - y_true), axis=-1)
-    y_pred_2d = K.reshape(y_pred, (7, 2, 257, 251, 2))
+    y_pred_2d = K.reshape(y_pred, (K.shape(y_pred)[0], 2, 257, 251, 2))
     combine_samples = K.sum(y_pred_2d, axis=0)
     combine_coeff = K.sum(combine_samples, axis=-1)
     combine_instr = K.sum(combine_coeff, axis=0)
     combine_freq = K.sum(combine_instr, axis=0)
     regularization = combine_freq[0] # get the time 0 total
 
-    regularization = K.print_tensor(regularization, message='worked?')
     return mse + 100 * regularization
